c26162404fe5dfa00185dbc932d26dea6578796c
ayush-triazine <ayushg@gmail.com>
2023-02-23 17:41:07 +0530

    code updated

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,7 +1,4 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
-import 'package:otp_auth/app/routes/app_pages.dart';
 
 class HomeController extends GetxController {
   // Get.toNamed(Routes.OTP_VERIFY);
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
-import 'package:intl_phone_field/intl_phone_field.dart';
 
 import '../controllers/home_controller.dart';
 
